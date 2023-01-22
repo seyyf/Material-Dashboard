5165e6c8664134cea865de6ef6e3e66df5af1476
brm.mohamed.brm@gmail.com <mejri.seyf@gmail.com>
2023-01-22 17:46:21 +0100

    Add redux

--- src/index.js
+++ src/index.js
@@ -13,18 +13,25 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
 import App from "App";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 // Material Dashboard 2 React Context Provider
 import { MaterialUIControllerProvider } from "context";
 
+const store = configureStore({
+  reducer: {},
+});
+
 ReactDOM.render(
   <BrowserRouter>
     <MaterialUIControllerProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </MaterialUIControllerProvider>
   </BrowserRouter>,
   document.getElementById("root")
---
+++ src/layouts/authentication/TestComponents/features/CandidatSlice.js
@@ -0,0 +1,62 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   comp1: {
+//     title: "Candidatures",
+//     number: "120",
+//   },
+//   comp2: {
+//     title: "Nouveaux profils",
+//     number: "64",
+//   },
+//   comp3: {
+//     title: "Nouveaux profils",
+//     number: "64",
+//   },
+//   comp4: {
+//     title: "Dossiers favoris",
+//     number: "535",
+//   },
+// };
+
+// export const comp1Slice = createSlice({
+//   name: "comp1",
+//   initialState: initialState.comp1,
+//   reducers: {
+//     comp: (state, action) =>{
+//         state.value = action.payload
+//     }
+//   },
+// });
+
+// export const comp2Slice = createSlice({
+//   name: "comp2",
+//   initialState: initialState.comp2,
+//   reducers: {
+//     comp: (state, action) =>{
+//         state.value = action.payload
+//     }
+//   },
+// });
+
+// export const comp3Slice = createSlice({
+//   name: "comp3",
+//   initialState: initialState.comp3,
+//   reducers: {
+//     comp: (state, action) =>{
+//         state.value = action.payload
+//     }
+//   },
+// });
+
+// export const comp4Slice = createSlice({
+//   name: "comp4",
+//   initialState: initialState.comp4,
+//   reducers: {
+//     comp: (state, action) =>{
+//         state.value = action.payload
+//     }
+//   },
+// });
+
+// export default comp1Slice.reducer, comp2Slice, comp3Slice, comp4Slice;
