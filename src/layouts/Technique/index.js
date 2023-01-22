fb01ee2b9e60589de9e788c7c2a154227d97e6dc
brm.mohamed.brm@gmail.com <mejri.seyf@gmail.com>
2023-01-22 04:29:31 +0100

    Annonces section

--- src/layouts/Technique/index.js
+++ src/layouts/Technique/index.js
@@ -1,6 +1,12 @@
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DriveFileRenameOutlineOutlinedIcon from "@mui/icons-material/DriveFileRenameOutlineOutlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import PauseOutlinedIcon from "@mui/icons-material/PauseOutlined";
+import PlayArrowOutlinedIcon from "@mui/icons-material/PlayArrowOutlined";
+import RunningWithErrorsOutlinedIcon from "@mui/icons-material/RunningWithErrorsOutlined";
 import { Box, Container, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import Annonce from "layouts/authentication/TestComponents/Annonce";
 import Candidat from "layouts/authentication/TestComponents/Candidat";
 import TestLayout from "../authentication/components/TestLayout";
 
@@ -39,13 +45,104 @@ function TestTechnique() {
                 <Candidat number="120" text="Candidatures" />
               </Box>
               <Box gridColumn="span 3" alignItems="center" justifyItems="center" display="flex">
-                <Candidat number="120" text="Candidatures" />
+                <Candidat number="64" text="Nouveaux profils" />
               </Box>
               <Box gridColumn="span 3" alignItems="center" justifyItems="center" display="flex">
-                <Candidat number="120" text="Candidatures" />
+                <Candidat number="64" text="Dossiers intéressants" />
               </Box>
               <Box gridColumn="span 3" alignItems="center" justifyItems="center" display="flex">
-                <Candidat number="120" text="Candidatures" />
+                <Candidat number="535" text="Dossiers favoris" />
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            coloredShadow="dark"
+            display="grid"
+            gridTemplateColumns="repeat(12, 1fr)"
+            gridAutoRows="100%"
+            gap="20px"
+          >
+            <Box
+              gridColumn="span 3"
+              display="flex"
+              flexDirection="column"
+              gap={2}
+              sx={{ background: "white", borderRadius: "0.5rem" }}
+              padding={2}
+            >
+              <Typography>Annonces</Typography>
+
+              <Box>
+                <Annonce
+                  icon={
+                    <PlayArrowOutlinedIcon
+                      sx={{ color: "#28C76F", background: "#00ba6224", borderRadius: "50px" }}
+                      fontSize="large"
+                    />
+                  }
+                  number="23"
+                  text="En ligne"
+                />
+                <Annonce
+                  icon={
+                    <DriveFileRenameOutlineOutlinedIcon
+                      sx={{
+                        color: "#62A5FF",
+                        background: "#4fc3f726",
+                        borderRadius: "50px",
+                        padding: "3px",
+                      }}
+                      fontSize="large"
+                    />
+                  }
+                  number="23"
+                  text="Brouillon"
+                />
+                <Annonce
+                  icon={
+                    <PauseOutlinedIcon
+                      sx={{
+                        color: "#6E6B7B",
+                        background: "#1e1e1e24",
+                        borderRadius: "50px",
+                        padding: "3px",
+                      }}
+                      fontSize="large"
+                    />
+                  }
+                  number="23"
+                  text="Désactivées"
+                />
+                <Annonce
+                  icon={
+                    <RunningWithErrorsOutlinedIcon
+                      sx={{
+                        color: "#EA5455",
+                        background: "#ff0a0026",
+                        borderRadius: "50px",
+                        padding: "3px",
+                      }}
+                      fontSize="large"
+                    />
+                  }
+                  number="23"
+                  text="Expirées"
+                />
+                <Annonce
+                  icon={
+                    <Inventory2OutlinedIcon
+                      sx={{
+                        color: "#5E5873",
+                        background: "#1e1e1e21",
+                        borderRadius: "50px",
+                        padding: "6px",
+                      }}
+                      fontSize="large"
+                    />
+                  }
+                  number="23"
+                  text="Archivées"
+                />
               </Box>
             </Box>
           </Box>
---
+++ src/layouts/authentication/TestComponents/Annonce.js
@@ -0,0 +1,30 @@
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Box, Button, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+function Annonce({ icon, number, text }) {
+  return (
+    <Box display="flex" alignItems="center" justifyContent="space-between" gap={2}>
+      <Box display="flex" gap={2.5}>
+        <Box marginTop={1}>{icon}</Box>
+        <Box display="flex" flexDirection="column">
+          <Typography sx={{ fontSize: "18px", fontWeight: "bold" }}>{number}</Typography>
+          <Typography sx={{ fontSize: "12px", fontWeight: "thin" }}>{text}</Typography>
+        </Box>
+      </Box>
+      <Box>
+        <Button sx={{ borderRadius: "5px", background: "#62A5FF", color: "#fff" }}>
+          <ArrowForwardIosIcon />
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+Annonce.propTypes = {
+  icon: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default Annonce;
